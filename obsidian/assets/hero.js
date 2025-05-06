46120865e5297afc7bc25d4b6be6828340b1b952
exec432 <exec432@gmail.com>
2025-05-06 17:43:28 +0200

    Added dynamic content on the hero page, when the Welcome animation stops, the full shop will show (when it will be done)

--- obsidian/assets/hero.js
+++ obsidian/assets/hero.js
@@ -1,15 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
   const heroTextWrapper = document.querySelector('.hero-text-wrapper');
+
   if (heroTextWrapper) {
     const text = heroTextWrapper.getAttribute('data-hero-title');
     heroTextWrapper.innerHTML = text
       .split('')
       .map((letter, index) => {
         if (letter === ' ') {
-          return `<span class="space" style="--index: ${index}">&nbsp;</span>`; // Add a span for spaces
+          return `<span class="space" style="--index: ${index}">&nbsp;</span>`;
         }
         return `<span style="--index: ${index}">${letter}</span>`;
       })
       .join('');
+
+    setTimeout(() => {
+      const spans = heroTextWrapper.querySelectorAll('span');
+      if (spans.length > 0) {
+        const lastSpan = spans[spans.length - 1];
+
+        lastSpan.addEventListener('animationend', () => {
+          const heroContent = document.querySelector('.hero-content');
+          const newContent = `
+            <h1 class="obsidian">OBSIDIAN</h1>
+          `;
+          heroContent.innerHTML = '';
+          heroContent.innerHTML += newContent;
+          heroContent.classList.add('fade-in');
+        });
+      }
+    }, 3000);
   }
 });
