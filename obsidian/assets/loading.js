77002e35a5541350ebcd17d54ee5de5d9fe9492c
exec432 <exec432@gmail.com>
2025-05-22 21:32:01 +0200

    Added the solution for not showing content when the loading section is not used.

--- obsidian/assets/loading.js
+++ obsidian/assets/loading.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const heroTextWrapper = document.querySelector('.loading-text-wrapper');
+  const loadingContent = document.querySelector('.loading-content');
+  const mainContent = document.querySelector('.main-content');
+  const htmlContainer = document.querySelector('html');
+  const productGridContainer = document.querySelector('.product-grid-container');
 
-  if (heroTextWrapper) {
+  if (loadingContent) {
     const text = heroTextWrapper.getAttribute('data-loading-title');
     heroTextWrapper.innerHTML = text
       .split('')
@@ -18,23 +22,30 @@ document.addEventListener('DOMContentLoaded', () => {
       const lastSpan = spans[spans.length - 1];
 
       lastSpan.addEventListener('animationend', () => {
-        const heroContent = document.querySelector('.loading-content');
-        const mainContent = document.querySelector('.main-content');
-        const htmlContainer = document.querySelector('html');
-        const productGridContainer = document.querySelector('.product-grid-container');
-
-        heroContent.classList.add('hidden');
+        loadingContent.classList.add('hidden');
         setTimeout(() => {
-          heroContent.style.display = 'none';
-          mainContent.style.display = 'block';
-          productGridContainer.style.display = 'block';
-          htmlContainer.style.overflow = 'auto';
+          loadingContent.style.display = 'none';
+          if (mainContent) mainContent.style.display = 'block';
+          if (productGridContainer) productGridContainer.style.display = 'block';
+          if (htmlContainer) htmlContainer.style.overflow = 'auto';
 
           requestAnimationFrame(() => {
-            mainContent.classList.add('shown');
+            if (mainContent) mainContent.classList.add('shown');
           });
         }, 500);
       });
+    }
+  } else {
+    // No loading content? Show everything immediately:
+    if (mainContent) {
+      mainContent.style.display = 'block';
+      mainContent.classList.add('shown');
+    }
+    if (productGridContainer) {
+      productGridContainer.style.display = 'block';
+    }
+    if (htmlContainer) {
+      htmlContainer.style.overflow = 'auto';
     }
   }
 });
